bb5f5998911a9aa3ab08d488e2f9326a794a2d57
Unknown <mikulasek@live.com>
2018-11-06 21:59:42 +0100

    Pcap Filter

--- pcpy/pcpy/main.cpp
+++ pcpy/pcpy/main.cpp
@@ -25,15 +25,22 @@ int main(int argc, char *argv[])
 			if (++i == 1) strcpy(ip, buf);
 			if (i == 2) strcpy(mac, buf);
 			if (i == 3) strcpy(interface, buf);
+			if (i == 4) strcpy(filter, buf);
 		}
 	}
 
 	fprintf(stdout, "\n%s", ip);
 	fprintf(stdout, "\n%s", mac);
 	fprintf(stdout, "\n%s", interface);
+	fprintf(stdout, "\n%s", filter);
 
 	if (pm->DeviceOpen(interface))
 	{
+		if (pm->SetFilter(filter))
+		{
+			fprintf(stdout, "\nfilter set");
+		}
+
 		pm->CopyTo(ip, mac);
 	}
 }
